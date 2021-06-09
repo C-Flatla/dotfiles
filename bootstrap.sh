217d8861980dbdc7c732ab850ba11b02bd09de55
Cody Flatla <cody.f@getjobber.com>
2021-06-08 19:40:16 -0600

    Run .mac_os in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 link_all () {
+  echo "Symlinking all dotfiles"
   for file in $( ls -A | grep -vE '\.sh|\.git$|\.gitignore|.*.md|LICENSE' ) ; do
     # Silently ignore errors here because the files may already exist
-    ln -sv "$PWD/$file" "$HOME" || true
+    ln -sf "$PWD/$file" "$HOME"
   done
 }
 
+configure_mac() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Configuring for Mac"
+    . .mac_os
+  fi
+}
+
 link_all
+configure_mac
+
+echo "Done :)"
